e4ce865fd47f55e3ae6879e2d1c23e4f04d7a476
Obinna Okpokwasili <limitless@Obinnas-MacBook-Pro-2.local>
2018-09-09 23:06:37 -0700

    pushing afresh

---
+++ backend.tf
@@ -0,0 +1,7 @@
+variable "repo_name" {}
+
+terraform {
+  backend "atlas" {
+    name = "obicarl/${var.repo_name}"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,39 @@
+variable "region" {}
+
+variable "subnet_id" {}
+
+variable "security_groups" {}
+
+variable "key_name" {}
+
+variable "key_file_path" {}
+
+variable "domain_name" {}
+
+# Linux Username to leverage when connecting
+variable "linux_username" {}
+
+variable "disk_sizes" {
+  type        = "list"
+}
+
+variable "lin_mount_points" {
+  type        = "list"
+}
+
+variable "type" {}
+
+variable "linux_password" {}
+
+variable "node_name" {}
+
+variable "redhat_ami" {}
+
+module "now" {
+  source  = "app.terraform.io/obicarl/now/testing"
+  version = "0.0.2"
+}
+
+# resource "null_resource" "n" {}
+
+#resource "null_resource" "a" {}
