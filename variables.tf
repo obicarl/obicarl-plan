160d3cc9ff933907f5cdab4aaa9bcaec0fe89e57
Obinna Okpokwasili <limitless@Obinnas-MacBook-Pro-2.local>
2018-09-10 10:45:32 -0700

    added variable blocks

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "region" {}
+
+variable "subnet_id" {}
+
+variable "security_groups" {}
+
+variable "key_name" {}
+
+variable "key_file_path" {}
+
+variable "domain_name" {}
+
+variable "linux_username" {}
+
+variable "disk_sizes" {}
+
+variable "lin_mount_points" {}
+
+variable "type" {}
+
+variable "linux_password" {}
+
+variable "node_name" {}
+
+variable "redhat_ami" {}
